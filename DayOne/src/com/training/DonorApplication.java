87cae267dde6988ed9f8898ba582a1b5a4ccb330
Madhura Satao <satao19.madhura@gmail.com>
2020-09-15 21:44:43 +0530

    Donor Assignment

--- DayOne/src/com/training/DonorApplication.java
+++ DayOne/src/com/training/DonorApplication.java
@@ -5,7 +5,6 @@ public class DonorApplication {
 	public static void main(String[] args) {
 		Donor first = new Donor("John Doe", "B+", 23, 62);
 		System.out.println(first.getDonorName());
-		System.out.println(first.getDonorAge());
 		System.out.println(first.getDonorWeight());
 		System.out.println(first.getDonorBloodGroup());
 		
